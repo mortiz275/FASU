db6e412286b9a3b73cb9031d0096092ffe56ed1e
Emily421 <54861125+Emily421@users.noreply.github.com>
2023-04-03 09:51:14 -0500

    Added time stamp printing

--- server2.c
+++ server2.c
@@ -58,7 +58,15 @@ void check_lost(){
 	node_t *current = head;
 	while(current != NULL){
 		if(difftime(time(NULL), current->last_ping) > LOST_THRESHOLD){
-			printf("\033[33mWarning: %s has not been communicating!\n", current->client_id);
+			time_t current_time;
+			struct tm * time_info;
+			char time_string[30];
+
+			time(&current_time);
+			time_info = localtime(&current_time);
+			strftime(time_string, sizeof(time_string), "[%a %b %d %Y %H:%M:%S]", time_info);
+
+			printf("\033[33m%s Warning: %s has not been communicating!\n", time_string, current->client_id);
 		}
 		current = current->next;
 	}
