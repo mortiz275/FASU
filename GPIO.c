682b0ee050125aa7d77b3d334de1a154f5eee528
drhealmore <sucydreams@hotmail.com>
2023-04-24 12:31:38 -0500

    Update GPIO.c

--- GPIO.c
+++ GPIO.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <wiringPi.h>
+//#include <wiringPi.h>
 #include <windows.h>
 #include <unistd.h>
 #include <time.h>
 
-#define GPIO_PIN 1
+//#define GPIO_PIN 1
 
-int main(void) {
+/*int main(void) {
     if (wiringPiSetup() == -1) {
         printf("Failed to initialize wiringPi\n");
         return 1;
@@ -31,5 +31,14 @@ int main(void) {
     // I assume from here we can then pipe these outputs to the client which will then push them to the server.
 
     return 0;
+}*/
+
+if (gpio_lev & (1 << 11)) {
+    printf("Currently receiving power.\n");
+    sleep(10);
+} else {
+    time_t t;
+    time(&t);
+    printf("Pin has lost power and gone offline at %s. Please check status.\n", ctime(&t));
 }
 
